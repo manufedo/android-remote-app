cfc525e0291582b6816c12df217433f736b705cb
Manuel Fedorovsky <manu.el.fedo@gmail.com>
2025-11-26 19:17:37 -0300

    Hago que se pueda elegir la temperatura, godines

--- app/src/main/java/com/example/remote/SurreyAcRemoteViewModel.kt
+++ app/src/main/java/com/example/remote/SurreyAcRemoteViewModel.kt
@@ -6,17 +6,44 @@ import androidx.compose.runtime.setValue
 
 class SurreyAcRemoteViewModel(private val _infraredTransmitter: InfraredTransmitter){
 
-    private val ON_COMMAND : Long = 0xB24D9F6040BF
+    private val COLD_COMMAND : Long = 0xB24D9F60000F
     private val OFF_COMMAND : Long = 0xB24D7B84E01F
     private val shortDuration = 550
     private val zeroDuration = shortDuration
     private val oneDuration = shortDuration*3;
     private val longDuration = shortDuration*8;
+
+    //Esto está hecho usando alguna variante del código gray
+    private val temperatureCode : Map<Int,Long> = hashMapOf(
+        17 to 0x00F0,
+        18 to 0x10E0,
+        19 to 0x30C0,
+        20 to 0x20D0,
+        21 to 0x6090,
+        22 to 0x7080,
+        23 to 0x50A0,
+        24 to 0x40B0,
+        25 to 0xC030,
+        26 to 0xD020,
+        27 to 0x9060,
+        28 to 0x8070,
+        29 to 0xA050,
+        30 to 0xB040
+    )
     var buttonPressed by mutableStateOf("")
         private set
 
-    var temperature by mutableStateOf(17)
-        private set
+    private var _temperature = mutableStateOf(17)
+
+    var temperature: Int
+        get() = _temperature.value
+        set(value) {
+            // Solo actualizamos y transmitimos si el valor es válido y diferente
+            if (value in 17..30 && value != _temperature.value) {
+                _temperature.value = value
+            }
+            setTemperature()
+        }
 
     fun onTemperatureUp() {
         if (temperature < 30) { // Assuming 30 is the max temp
@@ -32,12 +59,17 @@ class SurreyAcRemoteViewModel(private val _infraredTransmitter: InfraredTransmit
 
     fun onOnButtonPressed() {
         buttonPressed = "on"
-        transmitCommand(ON_COMMAND)
+        setTemperature()
     }
 
     fun onOffButtonPressed() {
         buttonPressed = "off"
          transmitCommand(OFF_COMMAND)
+    }
+
+    private fun setTemperature(){
+        val command = COLD_COMMAND or temperatureCode[temperature]!!
+        transmitCommand(command)
     }
 
      fun transmitCommand(command : Long){
